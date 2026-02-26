96415075b49d133e22519ceacf66f00d5504fea1
CharukeshPanjala <charukeshpanjala007@gmail.com>
2026-02-26 18:29:25 +0100

    error fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -39,8 +39,7 @@ return (
           </div>
           <button className="md:hidden">
             <svg className={`w-6 h-6 ${isScrolled ? 'text-retreat-green' : 'text-white'}`} fill="none" stroke="currentColor" viewBox="0 0 24 24">
-              <path strokeLinecap="round" strokeLinejo
-  in="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
             </svg>
           </button>
         </div>
